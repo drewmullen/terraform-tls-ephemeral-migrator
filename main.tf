75c0cf7b98f79f18645101a02e38e0274dd56ee4
Drew Mullen <drew.mullen@hashicorp.com>
2026-06-23 10:44:25 -0400

    init

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "tls_private_key" "legacy" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# resource "vault_kv_secret_v2" "legacy" {
+#   mount     = "kvv2"
+#   name      = "mytls"
+#   data_json = jsonencode({ private_key = tls_private_key.legacy.private_key_pem })
+# }
